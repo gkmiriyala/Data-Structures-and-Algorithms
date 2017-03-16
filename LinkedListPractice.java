52510c5ffd9adb1be24b485a18c9e3cc84d8d553
gkmiriyala <gkmiriyala@gmail.com>
2017-03-15 22:57:09 -0700

    update

--- LinkedListPractice.java
+++ LinkedListPractice.java
@@ -8,14 +8,16 @@ import helperClasses.RandLinkedList;
 public class LinkedListPractice {
 	public static void main(String[] args) {
 		MyLinkedList list = MyLinkedList.makeStringLL("String");
-		MyLinkedList.printLinkedList(revLinkedList(list));
+		MyLinkedList.printLinkedList(revLinkedListRecursively(list, list.head.next, list.head));
 		// System.out.println(isLinkedListPal(list));
 	}
 
 	/*
-	 * Reverse a Linked List
+	 * Reverse a Linked List iteratively  	
 	 */
 	public static MyLinkedList revLinkedList(MyLinkedList list) {
+		if (list.head==null)
+			return list;
 		ListNode curr = list.head.next;
 		ListNode prev = list.head;
 		while (curr.next != null) {
@@ -27,6 +29,27 @@ public class LinkedListPractice {
 		list.head.next = null;
 		list.head = curr;
 		list.head.next = prev;
+		return list;
+	}
+
+	/*
+	 * Reverse a Linked List recursively
+	 */
+	public static MyLinkedList revLinkedListRecursively(MyLinkedList list, ListNode curr, ListNode prev) {
+		if(list.head==null)
+			return list;
+		if (curr.next == null) {
+			list.head.next = null;
+			list.head = curr;
+			list.head.next = prev;
+		} else {
+			list.head.next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = list.head.next;
+			revLinkedListRecursively(list, curr, prev);
+		}
+
 		return list;
 	}
 
@@ -83,22 +106,22 @@ public class LinkedListPractice {
 		}
 		return slow;
 	}
-	
+
 	/*
 	 * Merge all branches of a linked list into a single linked list
 	 */
 	public RandLinkedList mergeLinkedList(RandLinkedList list) {
 		ListRandNode curr = list.head;
-		while (curr.random!=null || curr.next!=null) {
+		while (curr.random != null || curr.next != null) {
 			ListRandNode start = curr.random;
 			ListRandNode end = curr.random;
-			while (end.next!=null) {
-				end=end.next;
+			while (end.next != null) {
+				end = end.next;
 			}
 			end.next = curr.next;
 			curr.next = start;
-			curr.random=null;
-			curr=curr.next;
+			curr.random = null;
+			curr = curr.next;
 		}
 		return list;
 	}
--- StringsPractice.java
+++ StringsPractice.java
@@ -5,128 +5,156 @@ import java.util.HashMap;
 
 public class StringsPractice {
 	public static void main(String[] args) {
-		//System.out.println(numPal("abaaa"));
-		//System.out.println(isPalRecursion("abaab"));
-		//System.out.println(minCharForPal("abcebec"));
-		//System.out.print(getLongestCommonSubsequence("happy", "papy"));
-		System.out.print(strPermutation("hapy", "pyhap"));
+		// System.out.println(numPal("abaaa"));
+		// System.out.println(isPalRecursion("abaab"));
+		// System.out.println(minCharForPal("abcebec"));
+		// System.out.print(getLongestCommonSubsequence("happy", "papy"));
+		// System.out.print(strPermutation("hapy", "pyhap"));
+		System.out.print(strAllUniqueNoDataStruct("yhap"));
 	}
-	
+
 	/*
 	 * Given two strings, decide if one is a permutation of the other?
 	 */
 	public static boolean strPermutation(String sOne, String sTwo) {
-		if (sOne.length()!=sTwo.length()) return false;
+		if (sOne.length() != sTwo.length())
+			return false;
 		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-		for(int i = 0; i<sOne.length();i++)
-		{
+		for (int i = 0; i < sOne.length(); i++) {
 			if (!map.containsKey(sOne.charAt(i)))
 				map.put(sOne.charAt(i), 1);
-			else map.put(sOne.charAt(i), map.get(sOne.charAt(i))+1);
+			else
+				map.put(sOne.charAt(i), map.get(sOne.charAt(i)) + 1);
 		}
-		for (int j=0;j<sTwo.length();j++)
-		{
-			if (map.containsKey(sTwo.charAt(j)))
-			{
-				if (map.get(sTwo.charAt(j))>0)
-					map.put(sTwo.charAt(j), map.get(sTwo.charAt(j))-1);
-				else return false;
-			}
-			else return false;
-		}
-		return true;
-	}
-	
-	/*
-	 * Find all distinct palindromic substrings of a given string. Input: “abaaa”
-	 * Output: Below are 5 palindrome sub-strings
-	 * a, aa, aaa, aba, b
-	 */
-	public static int numPal(String str) {
-		String s = "";
-		ArrayList<String> set = new ArrayList<String>();
-		for (int i = 0; i<str.length();i++) {
-			for (int j=i; j<str.length();j++) {
-				s= s.concat(Character.toString(str.charAt(j)));
-				if(isPal(s) && !set.contains(s)) {
-					System.out.println(s);
-					set.add(s);
-				}	
-			}
-			s="";
-		}
-		return set.size();
-	}
-	
-	/*
-	 * Return whether a given String is a palindrome or not
-	 */
-	public static boolean isPal(String s) {
-		for (int i = 0; i<s.length(); i++) {
-			if (s.charAt(i)!=(s.charAt(s.length()-1-i)))
+		for (int j = 0; j < sTwo.length(); j++) {
+			if (map.containsKey(sTwo.charAt(j))) {
+				if (map.get(sTwo.charAt(j)) > 0)
+					map.put(sTwo.charAt(j), map.get(sTwo.charAt(j)) - 1);
+				else
+					return false;
+			} else
 				return false;
 		}
 		return true;
 	}
-	
+
+	/*
+	 * Find all distinct palindromic substrings of a given string. Input:
+	 * “abaaa” Output: Below are 5 palindrome sub-strings a, aa, aaa, aba, b
+	 */
+	public static int numPal(String str) {
+		String s = "";
+		ArrayList<String> set = new ArrayList<String>();
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i; j < str.length(); j++) {
+				s = s.concat(Character.toString(str.charAt(j)));
+				if (isPal(s) && !set.contains(s)) {
+					System.out.println(s);
+					set.add(s);
+				}
+			}
+			s = "";
+		}
+		return set.size();
+	}
+
 	/*
 	 * Return whether a given String is a palindrome or not
-	 * Use Recursion
+	 */
+	public static boolean isPal(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != (s.charAt(s.length() - 1 - i)))
+				return false;
+		}
+		return true;
+	}
+
+	/*
+	 * Return whether a given String is a palindrome or not Use Recursion
 	 */
 	public static boolean isPalRecursion(String s) {
-		if(s.length()<2)
+		if (s.length() < 2)
 			return true;
-		else if((s.charAt(0)!=s.charAt(s.length()-1)))
+		else if ((s.charAt(0) != s.charAt(s.length() - 1)))
 			return false;
-		return isPalRecursion(s.substring(1, s.length()-1));
+		return isPalRecursion(s.substring(1, s.length() - 1));
 	}
-	
+
 	/*
-	 * Return the minimum number of characters to add to a String to make it a palindrome
+	 * Return the minimum number of characters to add to a String to make it a
+	 * palindrome
 	 */
 	public static int minCharForPal(String s) {
 		int left = 0;
-		int right = s.length()-1;
-		while(left<right) {
-			if (s.charAt(left)!=s.charAt(right))
+		int right = s.length() - 1;
+		while (left < right) {
+			if (s.charAt(left) != s.charAt(right))
 				left++;
-			else break;
+			else
+				break;
 		}
-		int tempLeft=left;
+		int tempLeft = left;
 		int tempRight = right;
-		while (left<right) {
-			if (s.charAt(left)!=s.charAt(right)) {
+		while (left < right) {
+			if (s.charAt(left) != s.charAt(right)) {
 				return tempRight;
-			}
-			else {
+			} else {
 				left++;
 				right--;
 			}
 		}
 		return tempLeft;
 	}
-	
+
 	/*
 	 * Length of Longest common subsequence
 	 */
-	public static int getLongestCommonSubsequence(String a, String b){
+	public static int getLongestCommonSubsequence(String a, String b) {
 		int m = a.length();
 		int n = b.length();
-		int[][] dp = new int[m+1][n+1];
-	 
-		for(int i=0; i<=m; i++){
-			for(int j=0; j<=n; j++){
-				if(i==0 || j==0){
-					dp[i][j]=0;
-				}else if(a.charAt(i-1)==b.charAt(j-1)){
-					dp[i][j] = 1 + dp[i-1][j-1];
-				}else{
-					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+		int[][] dp = new int[m + 1][n + 1];
+
+		for (int i = 0; i <= m; i++) {
+			for (int j = 0; j <= n; j++) {
+				if (i == 0 || j == 0) {
+					dp[i][j] = 0;
+				} else if (a.charAt(i - 1) == b.charAt(j - 1)) {
+					dp[i][j] = 1 + dp[i - 1][j - 1];
+				} else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
 				}
 			}
 		}
-	 
+
 		return dp[m][n];
 	}
-	 
+
+	/*
+	 * Implement an algorithm to determine if a string has all unique
+	 * characters. What if you cannot use additional data Structures?
+	 */
+	public static boolean strAllUnique(String s) {
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			if (!map.containsKey(s.charAt(i)))
+				map.put(s.charAt(i), 1);
+			else
+				return false;
+		}
+		return true;
+	}
+
+	/*
+	 * Implement an algorithm to determine if a string has all unique characters
+	 * without using additional data structures
+	 */
+	public static boolean strAllUniqueNoDataStruct(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i + 1; j < s.length(); j++) {
+				if (s.charAt(i)==s.charAt(j))
+					return false;
+			}
+		}
+		return true;
+	}
 }
